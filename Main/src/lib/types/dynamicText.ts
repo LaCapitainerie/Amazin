ec4d762dee0ba5b5a9883057902a36d290ea6128
LaCapitainerie <hugo.antreassian@gmail.com>
2024-11-05 11:09:33 +0100

    Final Init

--- Main/src/lib/types/dynamicText.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod"
-
-export const DynamicTextSchema = z.object({
-    id: z.string(),
-    title: z.string(),
-    description: z.string(),
-})
-
-export type DynamicTextType = z.infer<typeof DynamicTextSchema>
